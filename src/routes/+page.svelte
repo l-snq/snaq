d78f5e2906adc6faf04ba3cc7cd0dcee96113584
le snaq <tastytothesupreme@gmail.com>
2025-04-09 15:21:52 -0600

    updated the app, and the html

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<h1>Hi, My name is Jayden, or you can call me SNQ.</h1>
+<p>I love to build things.</p>
+
+<ul>
+	<li>My <a href="https://github.com/l-snq">GITHUB</a></li>
+	<li><a href="https://github.com/l-snq/clawed/">a web scraper,</a> written in rust</li>
+	<li><a href="https://github.com/l-snq/dioxane/">a prototype game engine</a> i made also in rust</li>
+	<li><a href="https://github.com/l-snq/servicer-web-app/">servicer app,</a> I made this for the DevelopEd 3.0 Hackathon (we got first place in our category, nbd ;) )</li>
+
+</ul>
