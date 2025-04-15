46de6942f6b241d6cdebcca13cb80611442d66ba
snackkk <tastytothesupreme@gmail.com>
2025-04-14 20:03:36 -0600

    added image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,10 @@
 <h1>Hi, My name is Jayden, or you can call me SNQ.</h1>
 <p>I love to build things.</p>
 
+<script>
+
+	import dioxaneImage from '../lib/assets/dioxane.png'
+</script>
 <ul>
 	<li>My <a href="https://github.com/l-snq">GITHUB</a></li>
 	<li><a href="https://github.com/l-snq/clawed/">a web scraper,</a> written in rust</li>
@@ -19,6 +23,6 @@
 
 	<a href="https://github.com/l-snq/dioxane/">Dioxane:</a> A prototype game engine, made in Rust with WGPU (this was mainly
 	following a tutorial)
-	<img src="./assets/dioxane.png" alt="Dioxane, a game engine!"/>
+	<img src={dioxaneImage} style={{width: 40, height: 40}} alt="Dioxane, a game engine!"/>
 
-	</div>
+</div>
