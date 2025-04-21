d6e23fe57568c16d5b7f450cf5485b80c32d309e
lsnq <tastytothesupreme@gmail.com>
2025-04-20 23:19:55 -0600

    added html stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,27 +2,52 @@
 <p>I love to build things.</p>
 
 <script>
-
 	import dioxaneImage from '../lib/assets/dioxane.png'
 </script>
-<ul>
-	<li>My <a href="https://github.com/l-snq">GITHUB</a></li>
-	<li><a href="https://github.com/l-snq/clawed/">a web scraper,</a> written in rust</li>
-	<li><a href="https://github.com/l-snq/dioxane/">a prototype game engine</a> i made also in rust</li>
-	<li><a href="https://github.com/l-snq/servicer-web-app/">servicer app,</a> I made this for the DevelopEd 3.0 Hackathon (we got first place in our category, nbd ;) )</li>
+<div id="Content">
+	<ul>
+		<li>My <a href="https://github.com/l-snq">GITHUB</a></li>
+		<li><a href="https://github.com/l-snq/clawed/">a web scraper,</a> written in rust</li>
+		<li><a href="https://github.com/l-snq/dioxane/">a prototype game engine</a> i made also in rust</li>
+		<li><a href="https://github.com/l-snq/servicer-web-app/">servicer app,</a> I made this for the DevelopEd 3.0 Hackathon (we got first place in our category, nbd ;) )</li>
 
-</ul>
+	</ul>
 
-<h2> My Experience </h2>
+	<div id="Experience">
+		<h2>My Experience </h2>
 
-<p> </p>
+		<h3> Javascript Developer/Lead Estimator @ OpenStar </h3>
+		<p> 2024 - Present: Automates Excel scripting with VBA to improve workflow
+		and large data extraction from excel spreadsheets, as well as using
+		Javascript to add dynamic content to PDFs to ensure high quality estimates.
+		Also I do a lot of IT related stuff too here. It's a startup, afterall :P
+		</p>
+		<h3>Front End Developer @ TestPredikt</h3>
+		<p> 2021 - Present: Lead UI development and component functionality using Vue, and Flask
+		Python. Reduced Serverside compilation time by 5ms with optimizations to
+		dependency loading.</p>
 
-<div id="Projects">
+		<h3> Junior Full Stack Developer @ Impirica </h3>
+		<p> 2022 - 2023: Upgraded React web application from React 17 to 18,
+		implemented large scale features for mobile app, and managed DB upgrades
+		with React, Typescript C# ASP .NET, and Azure DevOps.</p>
 
-	<h2> Projects </h2>
+		<h3> Open Sourced Software Developer/IT support @ Aloha.pk/Openspades</h3>
+		<p> 2020 - 2021:
+			Lead team of four in codebase migration from Python2 to Python3, optimized
+			community forum by reducing use of JS and implemented serverside
+			rendering.
+		</p>
 
-	<a href="https://github.com/l-snq/dioxane/">Dioxane:</a> A prototype game engine, made in Rust with WGPU (this was mainly
-	following a tutorial)
-	<img src={dioxaneImage} style={{width: 40, height: 40}} alt="Dioxane, a game engine!"/>
+	</div>
 
+	<div id="Projects">
+
+		<h2> Projects </h2>
+
+		<a href="https://github.com/l-snq/dioxane/">Dioxane:</a> A prototype game engine, made in Rust with WGPU (this was mainly
+		following a tutorial)
+		<img src={dioxaneImage} width=800px alt="Dioxane, a game engine!"/>
+
+	</div>
 </div>
